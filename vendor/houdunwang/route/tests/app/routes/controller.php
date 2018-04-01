68e50d13ec5fcc996125888687ce14da4cf66f37
houdunwang <2300071698@qq.com>
2018-04-01 12:16:05 +0800

    优化

--- vendor/houdunwang/route/tests/app/routes/controller.php
+++
@@ -1,6 +0,0 @@
-<?php
-//控制器路由
-\houdunwang\route\Route::get('entry/show.html', 'controller\Entry@show'
-
-);
-\houdunwang\route\Route::controller('entry', "controller\Entry");
--- vendor/houdunwang/route/tests/app/routes/restful.php
+++
@@ -1,3 +0,0 @@
-<?php
-//资源路由
-\houdunwang\route\Route::resource('photo', "controller\Resource");
